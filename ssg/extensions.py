eede40c7773b742cb67bf18cc167be0b8e70f2be
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-08 12:36:00 +0200

    2.3 Adjust Sys path

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -4,5 +4,7 @@ import importlib
 from pathlib import Path
 
 
-def load_module(directory: str, name: str):
+def load_module(directory: str, name: str) -> None:
     sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
