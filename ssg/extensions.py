584272116aef42557417cd061d90a21f5cd11cb2
Balazs Heizer <heizer.balazs@gmail.com>
2021-08-08 12:47:26 +0200

    2.4 Load extensions directory

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -8,3 +8,8 @@ def load_module(directory: str, name: str) -> None:
     sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
+
+
+def load_directory(directory: Path) -> None:
+    for path in directory.rglob("*.py"):
+        load_module(directory.as_posix(), path.stem)
